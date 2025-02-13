9955b9acf3e12d905facd4df4793076382b9dc6c
Luiza Lorena Toscano de Medeiros <lorena.toscano@ifood.com.br>
2025-02-13 10:00:26 -0300

    feat(controllers): add authentication jwt

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from '@/env'
@@ -5,6 +6,10 @@ import { env } from '@/env'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
